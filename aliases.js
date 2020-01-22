156f770b86b7437d3f3abe9136f67c5a3a71886d
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2020-01-22 06:43:47 +0000

    Automated deployment: Wed Jan 22 06:43:47 UTC 2020 a8cc179469d20a40a61c5b9f46674ceb4e423c9d

--- aliases.js
+++ aliases.js
@@ -1,2 +1,2 @@
 var ALIASES = {};
-ALIASES["mimi"] = {};
+ALIASES["mimicaw"] = {};
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["mimi"] = {"name":"","files":["args.rs","driver.rs","lib.rs","progress.rs","test.rs"]};
+sourcesIndex["mimicaw"] = {"name":"","files":["args.rs","driver.rs","lib.rs","progress.rs","test.rs"]};
 createSourceSidebar();
