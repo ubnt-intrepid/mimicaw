74f0cb98a3a590b60031e3be1f1fd85eca2b0422
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-01-24 02:40:26 +0900

    update examples/many.rs

--- examples/many.rs
+++ examples/many.rs
@@ -1,40 +1,29 @@
 use futures::executor::block_on;
 use futures_timer::Delay;
 use mimicaw::{Args, Outcome, Test};
-use rand::{
-    distributions::{Bernoulli, Distribution},
-    seq::IteratorRandom,
-};
+use rand::distributions::{Bernoulli, Distribution, Uniform};
 use std::time::Duration;
 
 fn main() {
     let args = Args::from_env().unwrap_or_else(|st| st.exit());
 
+    let tests = (0..50).map(|i| Test::test(format!("test-{:03}", i), ()));
+
     let mut rng = rand::thread_rng();
     let bernoulli = Bernoulli::new(0.8).unwrap();
-    let intervals = &[2, 3, 5, 7];
+    let uniform = Uniform::from(500..=5000);
 
-    let tests = (0..20).map(|i| {
-        let name = format!("test-{:03}", i);
-        let interval = intervals.iter().choose(&mut rng).copied().unwrap_or(1);
-        let delay = Delay::new(Duration::from_secs(interval));
-        let outcome = if bernoulli.sample(&mut rng) {
-            Outcome::passed()
-        } else {
-            Outcome::failed()
-        };
-        Test::test(name, (delay, outcome))
-    });
-
-    block_on(mimicaw::run_tests(
-        &args,
-        tests,
-        |_desc, (delay, outcome)| {
-            Box::pin(async move {
-                delay.await;
-                outcome
-            })
-        },
-    ))
+    block_on(mimicaw::run_tests(&args, tests, |_desc, ()| {
+        let interval = uniform.sample(&mut rng);
+        let passed = bernoulli.sample(&mut rng);
+        async move {
+            Delay::new(Duration::from_millis(interval)).await;
+            if passed {
+                Outcome::passed()
+            } else {
+                Outcome::failed()
+            }
+        }
+    }))
     .exit();
 }
