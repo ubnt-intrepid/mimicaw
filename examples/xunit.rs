371b73ee5537c06ae7d7a98ea99385adea124075
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-01-24 23:23:22 +0900

    add support for reporting the test summary

---
+++ examples/xunit.rs
@@ -0,0 +1,106 @@
+use futures::executor::block_on;
+use mimicaw::{Args, Outcome, Report, Test};
+use std::{
+    fs::File,
+    io::{self, Write},
+    path::Path,
+};
+use sxd_document::{writer::Writer, Package};
+
+fn main() -> io::Result<()> {
+    let args = Args::from_env().unwrap_or_else(|st| st.exit());
+
+    let tests = vec![
+        Test::test("case1", "foo"),
+        Test::test("case2", "bar"),
+        Test::test("case3_long_computation", "baz").ignore(true),
+        Test::test("case4", "The quick brown fox jumps over the lazy dog."),
+    ];
+
+    let report = block_on(mimicaw::run_tests_with_report(
+        &args,
+        tests,
+        |_desc, data| {
+            async move {
+                match data {
+                    "foo" | "baz" => Outcome::passed(),
+                    "bar" => Outcome::failed().error_message("`bar' is forbidden"),
+                    data => Outcome::failed().error_message(format!("unknown data: {}", data)),
+                }
+            }
+        },
+    ))
+    .unwrap_or_else(|st| st.exit());
+
+    report_to_xml(&report, "report.xml")?;
+
+    report.status().exit()
+}
+
+fn report_to_xml(report: &Report, path: impl AsRef<Path>) -> io::Result<()> {
+    let package = Package::new();
+    let doc = package.as_document();
+
+    doc.root().append_child({
+        let testsuites = doc.create_element("testsuites");
+        testsuites.append_child({
+            let testsuite = doc.create_element("testsuite");
+            testsuite.set_attribute_value("name", "mimicaw xunit sample");
+            testsuite.set_attribute_value("timestamp", &chrono::Utc::now().to_rfc3339());
+            testsuite.append_child({
+                let properties = doc.create_element("properties");
+                properties.append_child({
+                    let property = doc.create_element("property");
+                    property.set_attribute_value("name", "module");
+                    property.set_attribute_value("value", module_path!());
+                    property
+                });
+                properties
+            });
+
+            for desc in &report.passed {
+                let testcase = doc.create_element("testcase");
+                testcase.set_attribute_value("name", desc.name());
+                testcase.append_child(doc.create_element("passed"));
+                testsuite.append_child(testcase);
+            }
+
+            for (desc, msg) in &report.failed {
+                let testcase = doc.create_element("testcase");
+                testcase.set_attribute_value("name", desc.name());
+                testcase.append_child({
+                    let failure = doc.create_element("failure");
+                    if let Some(msg) = msg {
+                        failure.set_attribute_value("message", &*msg);
+                    }
+                    failure
+                });
+                testsuite.append_child(testcase);
+            }
+
+            for (desc, reason) in report.skipped() {
+                let testcase = doc.create_element("testcase");
+                testcase.set_attribute_value("name", desc.name());
+                testcase.append_child({
+                    let skipped = doc.create_element("skipped");
+                    skipped.set_attribute_value("message", reason);
+                    skipped
+                });
+                testsuite.append_child(testcase);
+            }
+
+            testsuite
+        });
+
+        testsuites
+    });
+
+    let writer = Writer::new().set_single_quotes(false);
+
+    let mut file = File::create(path)?;
+    writer.format_document(&doc, &mut file)?;
+    writeln!(file)?;
+    file.flush()?;
+
+    Ok(())
+}
---
+++ src/report.rs
@@ -0,0 +1,82 @@
+use crate::{printer::Printer, test::TestDesc, ExitStatus};
+use std::{
+    borrow::Cow,
+    io::{self, Write},
+    sync::Arc,
+};
+
+/// A report on test suite execution.
+#[derive(Debug)]
+#[non_exhaustive]
+pub struct Report {
+    /// Passed test cases.
+    pub passed: Vec<TestDesc>,
+
+    /// Failed test cases with the error messages.
+    pub failed: Vec<(TestDesc, Option<Arc<Cow<'static, str>>>)>,
+
+    /// Benchmark results.
+    pub measured: Vec<(TestDesc, (u64, u64))>,
+
+    /// Test cases skipped because they do not satisfy the execution conditions.
+    pub ignored: Vec<TestDesc>,
+
+    /// Test cases filtered out.
+    pub filtered_out: Vec<TestDesc>,
+}
+
+impl Report {
+    /// Return an exit status used as a result of the test process.
+    pub fn status(&self) -> ExitStatus {
+        if self.failed.is_empty() {
+            ExitStatus::OK
+        } else {
+            ExitStatus::FAILED
+        }
+    }
+
+    /// Return an iterator of skipped test cases.
+    #[inline]
+    pub fn skipped(&self) -> impl Iterator<Item = (&TestDesc, &str)> + '_ {
+        let ignored = self.ignored.iter().map(|desc| (desc, "ignored"));
+        let filtered_out = self.filtered_out.iter().map(|desc| (desc, "filtered out"));
+        ignored.chain(filtered_out)
+    }
+
+    pub(crate) fn print(&self, printer: &Printer) -> io::Result<()> {
+        let mut status = printer.styled("ok").green();
+
+        if !self.failed.is_empty() {
+            status = printer.styled("FAILED").red();
+            writeln!(printer.term())?;
+            writeln!(printer.term(), "failures:")?;
+            for (desc, msg) in &self.failed {
+                writeln!(printer.term(), "---- {} ----", desc.name())?;
+                if let Some(msg) = msg {
+                    printer.term().write_str(&*msg)?;
+                    if msg.chars().last().map_or(true, |c| c != '\n') {
+                        printer.term().write_str("\n")?;
+                    }
+                }
+            }
+
+            writeln!(printer.term())?;
+            writeln!(printer.term(), "failures:")?;
+            for (desc, _) in &self.failed {
+                writeln!(printer.term(), "    {}", desc.name())?;
+            }
+        }
+
+        writeln!(printer.term())?;
+        writeln!(printer.term(), "test result: {status}. {passed} passed; {failed} failed; {ignored} ignored; {measured} measured; {filtered_out} filtered out",
+            status = status,
+            passed = self.passed.len(),
+            failed = self.failed.len(),
+            ignored = self.ignored.len(),
+            measured = self.measured.len(),
+            filtered_out = self.filtered_out.len(),
+        )?;
+
+        Ok(())
+    }
+}
