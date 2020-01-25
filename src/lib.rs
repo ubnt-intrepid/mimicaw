f56f434a7d97bc26e190ff48c4999c0fcc94a075
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-01-25 16:23:25 +0900

    disable readme deps test

--- src/lib.rs
+++ src/lib.rs
@@ -135,8 +135,3 @@ pub async fn run_tests<D>(
 fn test_html_root_url() {
     version_sync::assert_html_root_url_updated!("src/lib.rs");
 }
-
-#[test]
-fn test_readme_deps() {
-    version_sync::assert_markdown_deps_updated!("README.md");
-}
